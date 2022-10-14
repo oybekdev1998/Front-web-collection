df38ce502e59ca6cacb75c136452a781a16f0c42
oybekdev1998 <oybekjon.shamuratov@mail.ru>
2022-10-14 21:14:18 +0500

    Fixed bugs

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -4,7 +4,7 @@ import React from 'react';
 function EditProfile(props) {
     return (
         <div className={'container bg-slate-500 w-[100%] h-96'}>
-            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST8miLDKZypA0NexpmJ7b0MFZEIUyeWnUqOvaSuUQVng&s' />
+            <img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcST8miLDKZypA0NexpmJ7b0MFZEIUyeWnUqOvaSuUQVng&s' alt='profile' />
         </div>
     );
 }
